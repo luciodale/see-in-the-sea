0fd1f81e77ccdd8e7d5c9348f75ddeed2c6074ab
Lucio D'Alessandro <lda@juxt.pro>
2025-07-27 15:07:56 +0200

    add daisy ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,26 @@
 // @ts-check
-import cloudflare from "@astrojs/cloudflare";
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-vite-plugin';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://see-in-the-sea.lucio-dalessa.workers.dev",
+  site: 'https://see-in-the-sea.lucio-dalessa.workers.dev',
   integrations: [mdx(), sitemap(), react()],
   vite: {
-        plugins: [tanstackRouter(
-      {target: "react", autoCodeSplitting: true,
-            routesDirectory: "./src/react/routes",
-            generatedRouteTree: "./src/react/generated.ts",
-      }
-    ), tailwindcss()],
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+        routesDirectory: './src/react/routes',
+        generatedRouteTree: './src/react/generated.ts',
+      }),
+      tailwindcss(),
+    ],
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
       // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
@@ -26,9 +28,9 @@ export default defineConfig({
       //   "react-dom/server": "react-dom/server.edge",
       // } || {},
     },
-    },
+  },
   server: {
     open: true,
   },
-  adapter: cloudflare({imageService: 'cloudflare'}),
+  adapter: cloudflare({ imageService: 'cloudflare' }),
 });
