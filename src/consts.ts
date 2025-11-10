a2ad929b0b306f875382c6229749c8fced959e33
Lucio D'Alessandro <lda@juxt.pro>
2025-11-10 12:03:58 +0100

    Tweak text

--- src/consts.ts
+++ src/consts.ts
@@ -2,14 +2,15 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // English versions
-export const SITE_TITLE_EN = 'Underwater Photography Contest 2025';
+export const SITE_TITLE_EN = 'Worldwide Underwater Photography Contest 2025';
 export const SITE_DESCRIPTION_EN =
-  'Dive into creativity and showcase your best underwater photography across three stunning categories: Wide Angle, Macro, and Black & White.';
+  'Dive into creativity and showcase your best underwater photography across three stunning categories: Wide Angle, Macro, and Black & White. Join the International Underwater Photography Contest 2025.';
 
 // Italian versions
-export const SITE_TITLE_IT = 'Concorso di Fotografia Subacquea 2025';
+export const SITE_TITLE_IT =
+  'Concorso Internazionale di Fotografia Subacquea 2025';
 export const SITE_DESCRIPTION_IT =
-  'Immergiti nella creatività e mostra la tua migliore fotografia subacquea attraverso tre categorie mozzafiato: Grandangolo, Macro e Bianco e Nero.';
+  'Immergiti nella creatività e mostra la tua migliore fotografia subacquea attraverso tre categorie mozzafiato: Grandangolo, Macro e Bianco e Nero. Partecipa al Concorso Internazionale di Fotografia Subacquea 2025.';
 
 // Default versions (English)
 export const SITE_TITLE = SITE_TITLE_EN;
